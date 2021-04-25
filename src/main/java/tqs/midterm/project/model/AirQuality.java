691e9fc0136f60b24f0a3ec33d0509af394eab2b
Alexandra <alexandracarvalho@ua.pt>
2021-04-25 20:59:20 +0100

    Initial Commit

---
+++ src/main/java/tqs/midterm/project/model/AirQuality.java
@@ -0,0 +1,58 @@
+package tqs.midterm.project.model;
+
+public class AirQuality {
+    private float co;
+    private float no2;
+    private float o3;
+    private float so2;
+    private float pm25;
+    private float pm50;
+
+    public float getCo() {
+        return co;
+    }
+
+    public void setCo(float co) {
+        this.co = co;
+    }
+
+    public float getNo2() {
+        return no2;
+    }
+
+    public void setNo2(float no2) {
+        this.no2 = no2;
+    }
+
+    public float getO3() {
+        return o3;
+    }
+
+    public void setO3(float o3) {
+        this.o3 = o3;
+    }
+
+    public float getSo2() {
+        return so2;
+    }
+
+    public void setSo2(float so2) {
+        this.so2 = so2;
+    }
+
+    public float getPm25() {
+        return pm25;
+    }
+
+    public void setPm25(float pm25) {
+        this.pm25 = pm25;
+    }
+
+    public float getPm50() {
+        return pm50;
+    }
+
+    public void setPm50(float pm50) {
+        this.pm50 = pm50;
+    }
+}
